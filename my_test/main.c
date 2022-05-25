668970ff39a38dc4a5334307aed061c827295396
aime-franco <usanaseaimefrancois@gmail.com>
2022-05-25 11:58:15 +0200

    create main file

---
+++ my_test/main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0 success
+ */
+int main(void)
+{
+	int x;
+	int num;
+
+	number = 19912507;
+	x = _printf("%i, %d\n", num, num);
+	printf("%d\n", x);
+	x = _printf("% ");
+	printf("%d\n", a);
+	return (0);
+}
