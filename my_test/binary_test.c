9e607a9e21533e06b828ce3686514455a912a963
aime-franco <usanaseaimefrancois@gmail.com>
2022-05-25 11:05:52 +0200

    create binary test file

---
+++ my_test/binary_test.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Return: 0 success
+ */
+int main(void)
+{
+	int x;
+
+	x = _printf("%b\n", 0);
+	printf("%d\n", a);
+	return (0);
+}
